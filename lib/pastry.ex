1c4ca958b60f78152186d0d6e8b194cec3dcf5e8
Akshay Sharma <akshay.sharma@ufl.edu>
2017-10-22 17:28:07 -0400

    completed implementation of API

--- lib/pastry.ex
+++ lib/pastry.ex
@@ -1,71 +1,64 @@
 defmodule Pastry do
-  @moduledoc """
-  Documentation for Pastry.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Pastry.hello
-      :world
-
-  """
+  
   def main(args) do
     IO.puts "Welcome to Pastry"
     numNodes=String.to_integer(Enum.at(args,0))
     numRequests=String.to_integer(Enum.at(args,1))
     #IO.puts(numNodes)
     #IO.puts(numRequests)
-    nodeList=[]
-    createNodes(numNodes, numRequests, nodeList, 1)
+
+    #creating numNodes number of initial nodes
+    nodeList=createNodes(numNodes, numRequests, [], 1)
+    
+    #populating leaves set for initial nodes
+    randomStartNode=getRandomStartNode(nodeList)
+    
+    #Pastry.PastryNode.printNodes(randomStartNode)
+    Pastry.PastryNode.route("hi",pidMD5hash(:rand.uniform(numNodes*2)),randomStartNode)
+
+    unlimitedLoop
+
+    
   end
 
+  
+  
   #To be called first time when nodes of the pastry are supposed to be created
   def createNodes(numNodes, numRequests, nodeList, nodeNo) when nodeNo<=numNodes do
     #IO.puts nodeNo
     x=pidMD5hash(nodeNo)
-    Pastry.PastryNode.start_link(x)
+    Pastry.PastryNode.pastryInit(x,nil)
     nodeList=[x|nodeList]
     createNodes(numNodes, numRequests, nodeList, nodeNo+1)
   end
   #to be called when all nodes are created
   def createNodes(numNodes, numRequests, nodeList, nodeNo) when nodeNo>numNodes do
-    nodeList= Enum.sort(nodeList)
-    #IO.inspect nodeList
-    populateleafSet(0,numNodes,nodeList)
+    Enum.sort(nodeList)
   end
 
-  #populates the leafset of every node 
-  def populateleafSet(nodeNo,numNodes, nodeList) when nodeNo<numNodes do
-    :timer.sleep(1000)
-    #4 smaller smaller leaves 4
-    cond do
-    nodeNo==0 -> smallerLeafSet=[nil,nil,nil,nil]; IO.puts 1
-    nodeNo==1 -> smallerLeafSet=[nil,nil,nil,Enum.at(nodeList,nodeNo-1)]; IO.puts 2
-    nodeNo==2 -> smallerLeafSet=[nil,nil,Enum.at(nodeList,nodeNo-2),Enum.at(nodeList,nodeNo-1)]; IO.puts 3
-    nodeNo==3 -> smallerLeafSet=[nil,Enum.at(nodeList,nodeNo-3),Enum.at(nodeList,nodeNo-2),Enum.at(nodeList,nodeNo-1)]; IO.puts 4
-    true -> smallerLeafSet=[Enum.at(nodeList,nodeNo-4),Enum.at(nodeList,nodeNo-3),Enum.at(nodeList,nodeNo-2),Enum.at(nodeList,nodeNo-1)] 
-           IO.puts true
-    end
-    #4 larger smaller leaves
-    largerLeafSet=[Enum.at(nodeList,nodeNo+4),Enum.at(nodeList,nodeNo+3),Enum.at(nodeList,nodeNo+2),Enum.at(nodeList,nodeNo+1)]
-    Pastry.PastryNode.populateLeafSet(Enum.at(nodeList,nodeNo),smallerLeafSet,largerLeafSet)
-    populateleafSet(nodeNo+1,numNodes, nodeList)
+  
+  
+  def populateleafSet(nodeList, smallerLeaf, largerLeaf) when smallerLeaf<=largerLeaf do
+    mid=round(Float.floor((smallerLeaf+largerLeaf)/2))
+    Pastry.PastryNode.newLeaves(Enum.at(nodeList,mid),populateleafSet(nodeList,smallerLeaf,mid-1),populateleafSet(nodeList,mid+1,largerLeaf))
+    Enum.at(nodeList,mid)
   end
-  def populateleafSet(nodeNo,numNodes, nodeList) when nodeNo>=numNodes do
-    unlimitedLoop
+  def populateleafSet(nodeList, smallerLeaf, largerLeaf) when smallerLeaf>largerLeaf do
+    nil
   end
 
   #calling an unlimited loop to keep main thread alive
   def unlimitedLoop() do
-    unlimitedLoop()
+    :timer.sleep(:infinity)
   end
 
   #convert pid to its md5 hash
   def pidMD5hash(nodeNo)do
     :crypto.hash(:md5, Integer.to_string(nodeNo)) |> Base.encode16
+  end
+
+  def getRandomStartNode(nodeList) do
+    populateleafSet(nodeList,0,Enum.count(nodeList)-1)
   end
 
 end
--- lib/pastryNode.ex
+++ lib/pastryNode.ex
@@ -9,24 +9,91 @@ defmodule Pastry.PastryNode do
     def init(init_data) do
         {:ok,init_data} 
     end
-    
-    #Client callable functions
+
+    #Debugging the nodes ############################################################################################################
+    defp getSmallerLeafSet(nodeID) do
+        GenServer.call(nodeID,:getSmallerLeafSet)      
+    end
+
+    defp getLargerLeafSet(nodeID) do
+        GenServer.call(nodeID,:getLargerLeafSet)
+    end
+
+    def printNodes(nodeID) do
+        if (is_nil(nodeID)) do
+        else
+          printNodes (getSmallerLeafSet(String.to_atom(nodeID)))
+          IO.puts nodeID
+          printNodes (getLargerLeafSet(String.to_atom(nodeID)))
+        end        
+    end
+    ##############################################################################################################################
+
+    #implementing the pastry API
+
+    #pastryInit pastryInit(Credentials, Application):
+    #causes the local node to join an existing Pastry 
+    #network (or start a new one), initialize all relevant state, and return the local node’s nodeId. 
+    #The application-specific credentials contain information needed to authenticate the local node. 
+    #The application argument is a handle to the application object that provides the Pastry node with 
+    #the procedures to invoke whencertain events happen, e.g., a message arrival.
+    def pastryInit(credentials, application) do
+        start_link(credentials)
+    end  
 
     #populating leaf set
-    def populateLeafSet(pid, smallerLeafset, largerLeafSet) do
-        IO.puts "populate called"
-        GenServer.cast(String.to_atom(pid), {:populateLeafSet, smallerLeafset,largerLeafSet})
+    def newLeaves(pid, smallerLeafset, largerLeafSet) do
+        GenServer.cast(String.to_atom(pid), {:newLeaves, smallerLeafset,largerLeafSet})
         #GenServer.cast()
+    end 
+
+    #deliver(msg,key):
+    # called by Pastry when a message is received and the local node’s nodeId is numerically closest to key, among all live nodes.
+    def deliver(msg, key) do
+        IO.puts("Total number of hops required for delivery :"<>Integer.to_string(Map.get(msg,:hops)))
+        IO.puts("Message string :"<>Map.get(msg,:msg))
     end
 
+    #route(msg,key):
+    # causes Pastry to route the given message to the node with nodeId numerically closest to the key, among all live Pastry nodes.
+    def route(msg, key,nodeID) do
+        msgPacket = %{msg: msg, hops: 0}
+        nextID=nodeID
+        forward(msgPacket,key,nextID)
+    end
+
+    #forward(msg,key,nextId):
+    #Called by Pastry just before a message is forwarded to the  node  with nodeId = nextId. 
+    #The application may change the contents of the message or the value of nextId. 
+    #Setting the nextId to NULL terminates the message at the local node.
+    def forward(msg, key, nodeID)do
+        :timer.sleep(1000)
+        msg=Map.put(msg,:hops,Map.get(msg,:hops) +1)
+        cond do
+            (is_nil(nodeID) or nodeID==key) -> deliver(msg,key)
+            key>nodeID -> forward(msg,key,(getLargerLeafSet(String.to_atom(nodeID))))
+            key<nodeID -> forward(msg,key,(getSmallerLeafSet(String.to_atom(nodeID))))
+        end
+    end
+
+    ##################################################################################################################################    
+    
     #server side functions
-    def handle_cast({:populateLeafSet,smallerLeafset,largerLeafSet }, state) do
+    
+    #Populating the leaf set
+    def handle_cast({:newLeaves,smallerLeafset,largerLeafSet }, state) do
         state=Map.put(state,:smallerLeafset,smallerLeafset)
         state=Map.put(state,:largerLeafSet,largerLeafSet)
-        #IO.puts "smallerLeafSet for : "<> Map.get(state,:nodeID)
-        #IO.inspect Map.get(state,:smallerLeafset)
-        #IO.puts "largerLeafSet for : "<> Map.get(state,:nodeID)
-        #IO.inspect Map.get(state,:largerLeafSet)
         {:noreply, state}
     end
+
+    def handle_call(:getLargerLeafSet,_from,state) do
+        {:reply, Map.get(state,:largerLeafSet),state}
+    end
+
+    def handle_call(:getSmallerLeafSet,_from,state) do
+        {:reply, Map.get(state,:smallerLeafset),state}
+    end
+
+
 end
